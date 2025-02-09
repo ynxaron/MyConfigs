618b72d92f7e4a46cd2ea522e1965ac923323f9f
ynxaron <ynx.aron@outlook.com>
2025-02-09 11:53:52 +0530

    Adding a run.sh shell script that would contain all that would be needed to set up the enviroment

---
+++ run.sh
@@ -0,0 +1,6 @@
+# Initializing zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/main/install.sh | zsh
+
+# Linking .config/zsh to $HOME folder
+link ~/.config/zsh/zshrc ~/.zshrc
+link ~/.config/zsh/zshenv ~/.zshenv
